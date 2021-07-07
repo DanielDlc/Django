9731ae187b29601a5618bd7eda1f858e46ca0e03
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-07-07 10:42:54 -0300

    path views.investimentos

--- invista-me/projeto_invista_me/projeto_invista_me/urls.py
+++ invista-me/projeto_invista_me/projeto_invista_me/urls.py
@@ -20,11 +20,6 @@ from invista_me import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.pagina_inicial),
+    path("", views.investimentos),
     path("novo_investimento/", views.novo_investimento, name="novo_investimento"),
-    path(
-        "investimento_registrado/",
-        views.investimento_registrado,
-        name="investimento_registrado",
-    ),
 ]
