e928a16b426259e0665b422e1de2f72f50479f6f
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-07-07 10:39:06 -0300

    função investimentos

--- invista-me/projeto_invista_me/invista_me/views.py
+++ invista-me/projeto_invista_me/invista_me/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-
-
-def pagina_inicial(request):
-    return HttpResponse("Pronto para investir! Daniel.")
+from .models import Investimento
 
 
 def novo_investimento(request):
     return render(request, "investimentos/novo_investimento.html")
 
 
-def investimento_registrado(request):
-    print(request.POST.get("TipoInvestimento"))
-    investimento = {" tipo_investimento": request.POST.get("TipoInvestimento")}
-    return render(request, "investimentos/investimento_registrado.html", investimento)
+def investimentos(request):
+    dados = {"dados": Investimento.objects.all()}
+    return render(request, "investimentos/investimentos.html", context=dados)
