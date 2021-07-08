55ad53bbe2268e42174f8ac5badce37797f2ac10
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2021-07-08 11:18:54 -0300

    detalhes

--- invista-me/projeto_invista_me/invista_me/views.py
+++ invista-me/projeto_invista_me/invista_me/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Investimento
-
-
-def novo_investimento(request):
-    return render(request, "investimentos/novo_investimento.html")
+from .forms import InvestimentoForm
 
 
 def investimentos(request):
     dados = {"dados": Investimento.objects.all()}
     return render(request, "investimentos/investimentos.html", context=dados)
+
+
+def detalhe(request, id_investimento):
+    dados = {"dados": Investimento.objects.get(pk=id_investimento)}
+    return render(request, "investimentos/detalhe.html", dados)
+
+
+def criar(request):
+    if request.method == "POST":
+        investimento_form = InvestimentoForm(request, POST)
+        if investimento_form.is_valid():
+            investimento_form.save()
+        return redirect("investimentos")
+    else:
+        investimento_form = InvestimentoForm()
+        formulario = {"formulario": investimento_form}
+        return render(
+            request, "investimentos/novo_investimento.html", context=formulario
+        )
