734548c89ded7ff44710ddb2dfec071c47808c3c
DanielDlc <dlc.engcomputacao@gmail.com>
2021-07-12 14:49:17 -0300

    subindo aplicação para o heroku

--- invista-me/projeto_invista_me/README.md
+++ invista-me/projeto_invista_me/README.md
@@ -1,20 +1,21 @@
 # Invista-me
 
-
 ## Este projeto foi feito com:
 
-* Python 3.9.5
-* Django 3.2.5
+- Python 3.9.5
+- Django 3.2.5
+- Bootstrap
 
 ## Como rodar o projeto?
 
-* Clone este repositório.
-* Crie uma virtualenv com Python 3
-* Instale as dependências.
-* Rode as migrações
+- Clone este repositório.
+- Crie uma virtualenv com Python 3
+- Instale as dependências.
+- Rode as migrações
 
 abra o seu terminal e digite:
-```bash 
+
+```bash
 git clone git@github.com:DanielDlc/Django.git
 cd Django
 python3 -m venv .venv
--- invista-me/projeto_invista_me/projeto_invista_me/settings.py
+++ invista-me/projeto_invista_me/projeto_invista_me/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+import os
+import whitenoise
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-s@3j=byo%hl!s=m@=4!3_dgsbyk4pe)#6g%v#qb(os%z7wsop&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -82,6 +85,8 @@ DATABASES = {
     }
 }
 
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -120,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
