8d189f2a0fec89672181c20d5c8564b2bc17cddd
DanielDlc <dlc.engcomputacao@gmail.com>
2021-07-21 10:54:43 -0300

    adicionando clientes

--- tw_clientes/tw_clientes/settings.py
+++ tw_clientes/tw_clientes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clientes',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'tw_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tw_django_fundamentos',
+        'USER': 'root',
+        'PASSWORD': '953351',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
