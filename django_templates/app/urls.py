79581e907ab099e61bf836a2578f7adf23fa5540
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-05 10:25:58 -0300

    contact e about

--- django_templates/app/urls.py
+++ django_templates/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('home', index)
+    path('home', index, name='home'),
+    path('contact/<int:id>', contact, name='contact'),
+    path('about', about, name='about')
+
 ]
