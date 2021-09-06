05f09f6d66243f0f3af317c1027b3b4e4ed78df8
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-06 13:04:23 -0300

    laço for para iterar no dicionário

--- django_templates/app/views.py
+++ django_templates/app/views.py
@@ -14,7 +14,15 @@ def index(request):
         "telefone": "21-999183939",
         "email": "dlc.engcomputacao@gmail.com"
     }
-    return render(request, 'empresa/index.html', {'nome_empresa': nome_da_empresa, 'descricao_empresa': descricao_da_empresa, 'contato_empresa': contato_empresa})
+
+    cursos_home = {
+        "1": {"titulo": "Django ", "descricao": "Aprenda todo posicionamento no Google.!!"},
+        "2": {"titulo": "Flask ", "descricao": "Aprenda toda a base do FFlask Agora mesmo!!"},
+        "3": {"titulo": "Estrutura de dados", "descricao": "Aprenda Estrutura de dados Agora mesmo!!"}
+    }
+
+    return render(request, 'empresa/index.html', {'nome_empresa': nome_da_empresa, 'descricao_empresa': descricao_da_empresa,
+                                                  'contato_empresa': contato_empresa, 'cursos_home': cursos_home})
 
 
 def about(request):
