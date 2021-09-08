6fa58aea561a6e5489e9c177f4daddcd96061187
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-08 07:42:30 -0300

    página sobre separada em html

---
+++ django_templates/app/templatetags/meus_filtros.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='remover_letra')
+def remover_letra(var, letra):
+    return var.replace(letra, '')
+# DLCTecnologia
