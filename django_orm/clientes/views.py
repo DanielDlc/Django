f2c4cd28e6405438edbc251bca4c5ad0c36d51e4
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-09 07:21:57 -0300

    views

---
+++ django_orm/clientes/views.py
@@ -0,0 +1,47 @@
+from django.http import request
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Cliente
+from .forms import ClienteForm
+
+# Create your views here.
+
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes/lista_clientes.html', {
+        'clientes': clientes
+    })
+
+
+def inserir_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_clientes')
+    else:
+        form = ClienteForm()
+    return render(request, 'clientes/form_cliente.html', {'form': form})
+
+
+def listar_cliente_id(request, id):
+    cliente = Cliente.objects.get(id=id)
+    return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
+
+
+def editar_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    form = ClienteForm(request.POST or None, instance=cliente)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/form_cliente.html', {'form': form})
+
+
+def remover_cliente(request, id):
+    cliente = Cliente.objects.get(id=id)
+    if request.method == "POST":
+        cliente.delete()
+        return redirect('listar_clientes')
+    return render(request, 'clientes/confirma_exclusao.html', {'cliente': cliente})
