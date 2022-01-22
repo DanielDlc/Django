83f8feff7cf180a7006c006d7ff4c1cf15fc1e66
DanielDlc <dlc.engcomputacao@gmail.com>
2022-01-22 11:20:20 -0300

    views das receitas

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+#from django.shortcuts import render
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
