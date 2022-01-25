727831100c1de17feca87de858fb517325eaa3a3
DanielDlc <dlc.engcomputacao@gmail.com>
2022-01-25 09:09:54 -0300

    urls com espaço no comentário

--- curso-django-projeto1/recipes/urls.py
+++ curso-django-projeto1/recipes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from recipes.views import contato, home, sobre
 
 urlpatterns = [
-    path('', home), # home
-    path('contato/', contato), # /sobre/
-    path('sobre/', sobre), # /contato/
+    path('', home),  # home
+    path('contato/', contato),  # /sobre/
+    path('sobre/', sobre),  # /contato/
 ]
