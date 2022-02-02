3adf53bcc9804c2e1464956a79e9e79fb42d9dd0
DanielDlc <dlc.engcomputacao@gmail.com>
2022-02-02 06:51:30 -0300

    views usando apenas home

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
-
-#from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Daniel Dlc',
+    })
