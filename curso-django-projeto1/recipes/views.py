ce4d3a4312adcdd7e72d453a3ff8cd3d516d90b8
DanielDlc <dlc.engcomputacao@gmail.com>
2022-02-18 07:34:49 -0300

    make recipe

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Daniel Dlc',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Daniel Dlc',
+        'recipe': make_recipe(),
     })
