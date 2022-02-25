93efe3f44b0e40a24f235b70b36d1f18bf3456c9
DanielDlc <dlc.engcomputacao@gmail.com>
2022-02-25 09:13:50 -0300

    criando category em admin

--- curso-django-projeto1/recipes/admin.py
+++ curso-django-projeto1/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Category
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
