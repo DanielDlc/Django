f1a0e9b161ae9e055dfbecad98e83e4e121612cd
DanielDlc <dlc.engcomputacao@gmail.com>
2022-02-25 08:50:09 -0300

    criando models

--- curso-django-projeto1/recipes/models.py
+++ curso-django-projeto1/recipes/models.py
@@ -1,10 +1,12 @@
-from functools import _Descriptor
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
-class Recipe(models.model):
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
@@ -15,6 +17,12 @@ class Recipe(models.model):
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%y/%m/%d')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
