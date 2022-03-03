6b66a8a4245353ecb3dd6bae951d2e84742cc668
DanielDlc <dlc.engcomputacao@gmail.com>
2022-03-03 10:00:07 -0300

    views com category

--- curso-django-projeto1/recipes/urls.py
+++ curso-django-projeto1/recipes/urls.py
@@ -8,5 +8,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),  # home
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
